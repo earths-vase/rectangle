38ee248538c1a4ac5b31ac1dde6093605fffab23
Joe Walton <152475586+earths-vase@users.noreply.github.com>
2023-12-03 23:48:39 +0600

    Create index.py

---
+++ index.py
@@ -0,0 +1,11 @@
+# Simple Python script to calculate the area of a rectangle
+
+# Get user input for length and width
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+# Calculate the area of the rectangle
+area = length * width
+
+# Display the result
+print(f"The area of the rectangle with length {length} and width {width} is: {area}")
